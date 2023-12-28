45bb9bbb00b5c4ef42b499177a60bdeb039c7fcb
ZidniGz <143054285+ZidniGz@users.noreply.github.com>
2023-12-28 14:00:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const {default:conn, delay, useMultiFileAuthState, DisconnectReason}= require("baileys")
+const { Boom} = require("@hapi/boom")
+const app = require("express")()
+
+app.get("/", (req, res) => res.send("GET"))
+app.listen(3000)
+async function connectToWhatsApp() {
+	const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
+	const waConfig = {
+		auth: state,
+		printQRInTerminal: true
+	};
+	const sock = conn(waConfig);
+	
+	sock.ev.process(
+		async(events) => {
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+
+				if(connection === 'close') {
+					// reconnect if not logged out
+					const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
+					console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
+					// reconnect if not logged out
+					if (shouldReconnect) {
+						await delay(1000)
+						connectToWhatsApp();
+					}
+				}
+				//console.log('connection update', update)
+			}
+
+			// credentials updated -- save them
+			if(events['creds.update']) {
+				await saveCreds(state)
+			}
+
+			// messages received
+			if(events['messages.upsert']) {
+				const upsert = events['messages.upsert']
+				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+
+				// message received not group
+				if(upsert.messages[0].key.participant === undefined) {
+					// message declaration as text
+	 				const pesan = await upsert.messages[0].message
+					// read messages
+                    if (!pesan) return
+					await sock.readMessages(upsert.messages.map(m => m.key))
+					// convert to lowercase
+					const pesanMasuk = pesan.conversation.toLowerCase();
+					// if message contains 'ping'
+					if(pesanMasuk.includes('ping')) {
+						// send a reply
+						await sock.sendMessage(upsert.messages[0].key.remoteJid, { text: 'Pong' }, { quoted: upsert.messages[0] })
+					}
+				}
+			}
+		}
+	)
+	
+	return sock;
+
+}
+connectToWhatsApp()
